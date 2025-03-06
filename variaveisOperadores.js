57256401ff6a0e64c9b78b273119869d2b05538b
sarahtorres13 <sarahcavl2004@gmail.com>
2025-03-06 18:06:36 -0300

    variaveisOperadores.js

---
+++ variaveisOperadores.js
@@ -0,0 +1,15 @@
+let numA = 2.0;
+let numB = 3.14;
+let numC; //undefined
+
+numC = numA + numB;
+console.log(`Resultado da adição entre ${numA} e ${numB} é ${numC}`);
+
+numC = numA - numB;
+console.log(`Resultado da subtração entre ${numA} e ${numB} é ${numC}`);
+
+numC = numA * numB;
+console.log(`Resultado da multiplicação entre ${numA} e ${numB} é ${numC}`);
+
+numC = numA / numB;
+console.log(`Resultado da divisão entre ${numA} e ${numB} é ${numC}`);
