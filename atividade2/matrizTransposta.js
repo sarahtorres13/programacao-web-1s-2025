72b9d678007b20d5c60e23c6b12fc57744948726
sarahtorres13 <sarahcavl2004@gmail.com>
2025-03-13 09:23:24 -0300

    add resultado matriz

---
+++ atividade2/matrizTransposta.js
@@ -0,0 +1,28 @@
+function transporMatriz(A){
+    console.log(' Matriz original \n')
+    for(let i = 0; i < A.length; i++){
+        let linha = "";
+        for(let j = 0; j < A[i].length; j++){
+            linha += A[i][i] + "\t"; 
+        }
+        console.log(linha); 
+    }
+    console.log('\n Transposta \n');
+    // Percorre colunas primeiro(matriz trasposta)
+    for(let j = 0; j < A[0].length; j++ ){
+        let linha = "";
+        for(let i = 0; i < A.length; i++){
+            linha += A[i][i] + "\t"; 
+        }
+        console.log(linha); 
+    }
+}
+
+let matriz = [
+
+    [1,2],
+    [3,4],
+    [5,6]
+];
+
+transporMatriz(matriz);
