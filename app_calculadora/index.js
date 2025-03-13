a23b80a1f2d1ee058df4cdcad074f4c6c4d9041d
sarahtorres13 <sarahcavl2004@gmail.com>
2025-03-13 09:54:35 -0300

    add update

--- app_calculadora/index.js
+++ app_calculadora/index.js
@@ -1,12 +1,8 @@
-function somar(a, b){
-    return a + b;
-}
-function subtrair(a, b){
-    return a - b;
-}
-function multiplicar(a, b){
-    return a * b;
-}
-function dividir(a, b){
-    return a / b; 
-}
+
+const calc = require('./calculadora');
+
+
+console.log(calc.somar(3, 5)); 
+console.log(calc.subtrair(3, 5));
+console.log(calc.multiplicar(3, 5));
+console.log(calc.dividir(3, 5));
