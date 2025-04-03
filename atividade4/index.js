42049a249a61a52d37b75f5b40457f10de81c45f
sarahtorres13 <sarahcavl2004@gmail.com>
2025-04-03 16:02:48 -0300

    add full code

--- atividade4/index.js
+++ atividade4/index.js
@@ -1,22 +1,23 @@
 const express = require('express');
-const app   = express();
+const estoque = require('./estoque');
+const app     = express();
 
 app.get('/', (req, res)=>{
+    let html = '<h1>app_estoque</h1>';
+    html += '<h3>Rotas disponíveis</h3>';
+    html += '<p>/adicionar/:id/:nome/:qtd - Adiciona um novo produto ao estoque</p>';
+    html += '<p>/listar - Lista todos os produtos do estoque</p>';
+    html += '<p>/remover/:id - Remove um produto do estoque</p>';
+    html += '<p>/editar/:id/:qtd - Altera a quantidade de um produto do estoque</p>';
 
-let html = '<h1>app_estoque</h1>'; 
-html += '<h3>Rotas disponíveis</h3>';
-html += 'socorro'
-html += 'adeus'
-html += 'a'
-res.send (html); 
+    res.send(html);
+});
 
-})
-
+// /adicionar/:id/:nome/:qtd
 app.get('/adicionar/:id/:nome/:qtd', (req, res)=>{
-
-    const id = Number(req.params.id);
+    const id   = Number(req.params.id);
     const nome = req.params.nome;
-    const qtd = Number(req.params.qtd);
+    const qtd  = Number(req.params.qtd);
 
     const item = {
         id: id,
@@ -24,40 +25,28 @@ app.get('/adicionar/:id/:nome/:qtd', (req, res)=>{
         qtd: qtd
     }
 
-    estoque.adicionar(item); 
-
-    res.send(item);
+    res.send(estoque.adicionar(item));
 });
 
+// /listar
 app.get('/listar', (req, res)=>{
+   res.send(estoque.listar());
+});
 
-
-
+// /remover/:id
+app.get('/remover/:id', (req, res)=>{
 
 });
 
-
-
-app.get('/remove/:id', (req, res)=> {
-
-
-
-
+// /editar/:id/:qtd
+app.get('/editar/:id/:qtd', (req, res)=>{
+   const id  = Number(req.params.id);
+   const qtd = Number(req.params.qtd);
+   
+   res.send(estoque.editar(id, qtd));
 });
-
-
-app.get('/editar/:id/:qtd', (req, res)=> {
-
-
-
-
-});
-
-
-
 
 const PORT = 8080;
 app.listen(PORT, ()=>{
-    console.log('app rodando na porta' + PORT); 
-
-}); 
+    console.log('app rodando na porta ' + PORT);
+});
