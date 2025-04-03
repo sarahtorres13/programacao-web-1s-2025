4334d07f421de70e692537ae2538cde60b5e6c53
sarahtorres13 <sarahcavl2004@gmail.com>
2025-04-03 09:40:58 -0300

    add changes

--- atividade4/estoque.js
+++ atividade4/estoque.js
@@ -1,12 +1,25 @@
 const itens = [];
 
 export function adicionar(item){
-itens.push(item);
-
+    let itens_valido = true;
+    if(isnan(item.id))
+    item_valido = false; 
 }
+    if(item_valido){
+
+        itens.push(item)
+    }
+
 
 export function listar(){
 
+return itens;
 
-    
+}
+
+module.exports = {
+    adicionar,
+    listar
+
+
 }
