1d041a6e8991eb95286940e6897795f745e5a8af
sarahtorres13 <sarahcavl2004@gmail.com>
2025-06-12 08:54:01 -0300

    update

--- app_agendamento_consulta/index.js
+++ app_agendamento_consulta/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
+const session = require('express-session');
+const db  = require('./db');
 const app = express();
 
 app.engine('html', mustacheExpress());
@@ -7,8 +9,19 @@ app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 app.use(express.urlencoded({extended: true}));
 
+app.use(session({
+    secret: 'secret-token',
+    name: 'sessionId',
+    resave: false,
+    saveUninitialized: false
+}));
+
 const agendamentoRouter = require('./routers/agendamentoRouter');
+const usuarioRouter = require('./routers/usuarioRouter');
 app.use('/', agendamentoRouter);
+app.use('/', usuarioRouter);
+
+db.sync();
 
 const PORT = 8080;
 app.listen(PORT, ()=>{
